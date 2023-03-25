1affe8c75f2dedb42d0a24a928182db8bfbd54dc
Dmitriy <dimkkononov@gmail.com>
2023-03-25 22:59:57 +0300

    add changes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
