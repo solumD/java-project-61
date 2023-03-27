32df6e9ba01625a77a31e41bf6814b9f919f772f
Dmitriy <dimkkononov@gmail.com>
2023-03-27 19:43:36 +0300

    add the final version of brain-even

--- app/src/main/java/hexlet/code/GameEven.java
+++ app/src/main/java/hexlet/code/GameEven.java
@@ -1,7 +1,42 @@
 package hexlet.code;
+import java.util.Random;
+import java.util.Scanner;
 
 public class GameEven {
     public static void even() {
         var user = Cli.greeting();
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        var j = 0;
+        while (j < 3) {
+            Random random = new Random();
+            int ch = random.nextInt(100);
+            var diff = ch % 2;
+            System.out.println("Question: " + ch);
+            System.out.print("Your answer: ");
+            Scanner scanner = new Scanner(System.in);
+            String answer = scanner.next();
+            if (diff == 1 && answer.trim().equals("no")) {
+                j += 1;
+                System.out.println("Correct!");
+            }
+            if (diff == 0 && answer.trim().equals("yes")) {
+                j += 1;
+                System.out.println("Correct!");
+            }
+            if (diff == 1 && !answer.trim().equals("no")) {
+                System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was 'no'.");
+                System.out.println("Let's try again, " + user + "!");
+                break;
+            }
+            if (diff == 0 && !answer.trim().equals("yes")) {
+                System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was 'yes'.");
+                System.out.println("Let's try again, " + user + "!");
+                break;
+            }
+
+        }
+        if (j == 3) {
+            System.out.println("Congratulations, " + user + "!");
+        }
     }
 }
