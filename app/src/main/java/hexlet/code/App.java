3cf0fccbc8493f1cb156b74c3232b74c464a020a
Dmitriy <dimkkononov@gmail.com>
2023-03-28 00:46:31 +0300

    add new brain-engine

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,37 +1,8 @@
 package hexlet.code;
-import hexlet.code.games.GameCalc;
-import hexlet.code.games.GameGCD;
-import hexlet.code.games.GameProgression;
-
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n"
-                + "4 - GCD\n5 - Progression\n0 - Exit");
-        System.out.print("Your choice: ");
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.next();
-        System.out.println();
-        if (command.equals("1")) {
-            Cli.greeting();
-        }
-        if (command.equals("0")) {
-            System.exit(0);
-        }
-        if (command.equals("2")) {
-            GameEven.even();
-
-        }
-        if (command.equals("3")) {
-            GameCalc.calc();
-        }
-        if (command.equals("4")) {
-            GameGCD.gcd();
-        }
-        if (command.equals("5")) {
-            GameProgression.progression();
-        }
+        Engine.engine();
     }
 }
 
