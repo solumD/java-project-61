9b1aa538a627e16eeb1d68aed6382a14f0a5fea6
Dmitriy <dimkkononov@gmail.com>
2023-03-28 18:55:44 +0300

    bug-fixes

--- app/src/main/java/hexlet/code/games/GameProgression.java
+++ app/src/main/java/hexlet/code/games/GameProgression.java
@@ -37,6 +37,7 @@ public class GameProgression {
             } else {
                 System.out.println("'" + answer + "'" + " is wrong answer ;(. Correct answer was " + "'" + qw + "'.");
                 System.out.println("Let's try again, " + user + "!");
+                j = 3;
                 break;
             }
         }
